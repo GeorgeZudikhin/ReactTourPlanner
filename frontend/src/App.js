61aa4f6c460ad3b5bde5c8c4356e78ada88036ea
GeorgeZudikhin <yehorzudikhin17@gmail.com>
2024-05-30 18:02:06 +0200

    added frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+import './App.css';
+import { useMemo, useState } from 'react';
+
+
+function App() {
+    return (
+    <div>
+      Hello world
+    </div>
+  );
+}
+
+export default App;
